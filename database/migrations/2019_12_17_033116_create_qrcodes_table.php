0f680de7771f69ebcf21f5fd834a90c7d425c8bc
seyi olawepo <sheyi103@outlook.com>
2019-12-17 07:13:07 +0100

    migration for roles transaction and qrcode

---
+++ database/migrations/2019_12_17_033116_create_qrcodes_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateQrcodesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('qrcodes', function (Blueprint $table) {
+            $table->Increments('id');
+            $table->integer('user_id');
+            $table->string('website')->nullable();
+            $table->string('company_name');
+            $table->string('product_name');
+            $table->string('product_url')->nullable();
+            $table->string('qrcode_path')->nullable();  // Path to Where QRCODE is saved on our server
+            $table->string('callback_url');
+            $table->float('amount', 10, 4);
+            $table->tinyInteger('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('qrcodes');
+    }
+}
